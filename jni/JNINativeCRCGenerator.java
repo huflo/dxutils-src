57f5f00f2d1a4d0363ff50995c21ea7219994996
Kevin Beineke <kevin.beineke@hhu.de>
2018-02-27 14:18:49 +0100

    CRC: split hash method into hashNative (zero-copy) and hashHeap

--- jni/JNINativeCRCGenerator.java
+++ jni/JNINativeCRCGenerator.java
@@ -30,6 +30,21 @@ public final class JNINativeCRCGenerator {
      * @param p_checksum
      *         the current checksum, 0 if all data is given at once.
      * @param p_data
+     *         the native address of the data to hash.
+     * @param p_offset
+     *         the offset within given array.
+     * @param p_length
+     *         the length of the data to hash.
+     * @return the CRC32C checksum.
+     */
+    public static native int hashNative(final int p_checksum, final long p_data, final int p_offset, final int p_length);
+
+    /**
+     * Update CRC32 checksum.
+     *
+     * @param p_checksum
+     *         the current checksum, 0 if all data is given at once.
+     * @param p_data
      *         the byte array with data to hash.
      * @param p_offset
      *         the offset within given array.
@@ -37,5 +52,5 @@ public final class JNINativeCRCGenerator {
      *         the length of the data to hash.
      * @return the CRC32C checksum.
      */
-    public static native int hash(final int p_checksum, final byte[] p_data, final int p_offset, final int p_length);
+    public static native int hashHeap(final int p_checksum, final byte[] p_data, final int p_offset, final int p_length);
 }
